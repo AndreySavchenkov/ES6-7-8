bd15715d997a8692503f3493fafdf858f47cb5a7
Andrey <sannndvik16@gmail.com>
2020-09-03 08:06:20 +0300

    spread and reduce

--- ES6-7-8.js
+++ ES6-7-8.js
@@ -1,4 +1,6 @@
 //---------------------------------let const------------------------------------------------------------------------------
+
+
 // let a = 24;
 
 // if(true) {
@@ -34,7 +36,10 @@
 // array.push(13)
 // console.log(array)
 
+
 // ----------------------------------------arrow functions-------------------------------------------------------
+
+
 // function sum(a,b) {
 //     return a + b
 // }
@@ -71,7 +76,9 @@
 
 // person.delayLog();
 
+
 //--------------------------------------------------default params----------------------------------------------------------
+
 
 // const defaultB = 30;
 // const getDefault = c => c * 2
@@ -82,7 +89,11 @@
 
 // console.log(compute(20))
 
+
+
 //---------------------------------------------string-------------------------------------------------------------------
+
+
 // const title = 'Hello';
 
 // const isVisible = () => Math.random() > 0.5
@@ -109,6 +120,32 @@
 // console.log(str.padStart(10, '1234'));
 // console.log(str.padEnd(8, 'abc'));
 
+//-----------------------------------------------rest spred---------------------------------------------------------------
+
+//  Rest
+//  function average(a, ...args) {
+    
+//    return args.reduce((acc, i) => acc += i, 0) / args.length
+//  }
+
+//  console.log(average(10,20,30,40,50))
+
+//Spread
+
+//const array = [1,2,3,5,8,13]
+//console.log(...array)
+//console.log(Math.max(...array))
+
+//const fib = [1, ...array]
+//console.log(fib)
+
+//Destructuring
+//const array = [1, undefined, 3, 5, 8, 13]
+
+// const a = array[0]
+// const b = array[1]
+
+//Object
 
 
 
