e14ca6c78aafaf966e89ab8f9f391d94fe2a26a4
Andrey <sannndvik16@gmail.com>
2020-09-06 20:47:36 +0300

    finished work with objects

--- 2_objects.js
+++ 2_objects.js
@@ -1,6 +1,12 @@
+const cityField = 'city'
+
+const job = 'Frontend'
+
 const person = {
     age: 26,
     name: 'Irina',
+    job,
+    [cityField]: 'Saint-Peterburg',
     'country-live': 'Russia',
     print: () => 'Person',
     toString() {
@@ -12,5 +18,20 @@ const person = {
     }
 }
 
-console.log(person.toString())
-console.log(person.print())
+// console.log(person.toString())
+// console.log(person.print())
+// console.log(person)
+
+//----------------------------------------Methods-------------------------------------------------
+ const first = {a: 1}
+ const second = {b: 2}
+
+// //console.log(Object.is(20, 20))
+ const obj = Object.assign({}, first, second, {c: 3, d: 4})
+// console.log(first)
+//console.log(obj)
+
+console.log(Object.entries(obj))
+console.log(Object.keys(obj))
+console.log(Object.values(obj))
+
