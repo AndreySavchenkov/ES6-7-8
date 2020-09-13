b3dde2d91eced2a666e1aab16e0467260bc70821
Andrey <sannndvik16@gmail.com>
2020-09-13 08:24:32 +0300

    map and set

--- 3_modules/7_promises.js
+++ 3_modules/7_promises.js
@@ -5,10 +5,36 @@
 // }) 
 
 const delay = ms => new Promise (((resolve, reject) => {
-    setTimeout(() => resolve('Done'), ms)
+    setTimeout(() => resolve(`Done ${ms}`), ms)
 }))
 
-delay(1000)
-    .then(data => delay(500))
-    .then(data => console.log(data))
+// delay(1000)
+//     .then(data => delay(500))
+//     .then(data => console.log(data))
+//     .catch(err => console.log(err))
+//     .finally(() => console.log('Finaly'))
 
+
+async function asyncDelay() {
+    try {
+        const data = await delay(2000)
+        console.log(data)
+    } catch(e) {
+        console.log('Error', e)
+    }
+   
+}
+
+asyncDelay()
+
+Promise.all([
+    delay(1000),
+    delay(500),
+    delay(2000)
+]).then(data => console.log(data))
+
+Promise.race([
+    delay(1000),
+    delay(500),
+    delay(2000)
+]).then(data => console.log(data))
